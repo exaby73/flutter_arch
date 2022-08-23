078cb1e0ad82bb86c69813e010304fdcca21a82b
Nabeel Parkar <nabeelparkar99@gmail.com>
2022-08-24 02:33:48 +0530

    fix: make_env.dart

--- bin/make_env.dart
+++ bin/make_env.dart
@@ -3,6 +3,8 @@ import 'package:dart_dotenv/dart_dotenv.dart';
 void main(List<String> args) {
   final env = DotEnv();
 
+  if (!env.exists()) env.createNew();
+
   for (final arg in args) {
     final values = arg.split('=');
     final key = values.first;
